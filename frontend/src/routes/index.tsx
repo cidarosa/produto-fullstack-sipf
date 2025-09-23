4c04b2d82bbfc3e9ff675867d12ad5626e6a0bd0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-23 11:40:43 -0300

    feat: ListarProdutos

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import ListarCategorias from "../pages/categorias/listar-categorias";
+import ListarProdutos from "../pages/produtos/listar-produtos";
 
 export default function AppRoutes(){
 
@@ -10,6 +11,7 @@ export default function AppRoutes(){
             <Route path="/" element={<h1>Página Inicial</h1>} />
 
             <Route path="/categorias" element={<ListarCategorias />} />           
+            <Route path="/produtos" element={<ListarProdutos />} />           
 
         </Routes>
     );
