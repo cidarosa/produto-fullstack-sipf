cc507b635a7308fec4a39900e286cbc325e0928f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-30 11:06:12 -0300

    feat: add loading, error states, atualizado useEffect, table

--- frontend/src/pages/categorias/listar-categorias/index.tsx
+++ frontend/src/pages/categorias/listar-categorias/index.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   Box,
+  CircularProgress,
   Paper,
   Table,
+  TableBody,
   TableCell,
   TableContainer,
   TableHead,
@@ -13,37 +15,67 @@ import { useEffect, useState } from "react";
 import type { CategoriaDTO } from "../../../models/categoria";
 
 import * as categoriaService from "../../../services/categoria-service.ts";
+import axios from "axios";
 
 export default function ListarCategorias() {
   const [categorias, setCategorias] = useState<CategoriaDTO[]>([]);
 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
     const fetchCategorias = async () => {
-      const data = await categoriaService.findAll();
-      setCategorias(data);
+      try {
+        const data = await categoriaService.findAll();
+        setCategorias(data);
+      } catch (error: unknown) {
+        let msg = "Erro ao carregar Categorias!";
+        if (axios.isAxiosError(error) && error.response) {
+          msg = error.response.data.error || msg;
+        }
+        setError(msg);
+      } finally {
+        setLoading(false);
+      }
     };
+    fetchCategorias();
   }, []);
 
   return (
     <Box sx={{ p: 4 }}>
       <Typography variant="h4" component="h1" gutterBottom>
-        Página de Listagem de Categorias
+        Listagem de Categorias
       </Typography>
 
-      <Typography variant="body1">
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>ID</TableCell>
-                <TableCell>Nome</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableCell>1</TableCell>
-            <TableCell>Eletrônicos</TableCell>
-          </Table>
-        </TableContainer>
-      </Typography>
+      {loading ? (
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        !error && (
+          <Typography variant="body1">
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Nome</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {categorias.map((categoria) => (
+                    <TableRow key={categoria.id}>
+                      <TableCell>{categoria.id}</TableCell>
+                      <TableCell>{categoria.nome}</TableCell>
+                    </TableRow>
+                  ))}
+
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Typography>
+        )
+      )}
     </Box>
   );
 }
