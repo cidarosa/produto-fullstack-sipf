f9e79219bf8dc48367747ca0d31d732da6ff73eb
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-14 11:29:30 -0300

    feat: editar-categoria, completo

--- frontend/src/pages/categorias/editar-categoria/index.tsx
+++ frontend/src/pages/categorias/editar-categoria/index.tsx
@@ -1,16 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Box, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Alert, Box, Button, TextField, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import type { CategoriaDTO } from "../../../models/categoria";
-import { findById } from "../../../services/categoria-service";
+import { findById, update } from "../../../services/categoria-service";
 import axios from "axios";
+
+type FormData = {
+  nome: string;
+};
 
 export default function EditarCategoria() {
   const { categoriaId } = useParams();
 
   const [categoria, setCategoria] = useState<CategoriaDTO>();
+
   const [error, setError] = useState<string | null>(null);
+
+  const [success, setSuccess] = useState<string | null>(null);
+
+  const [formData, setFormData] = useState<FormData>({ nome: "" });
 
   const navigate = useNavigate();
 
@@ -20,6 +29,7 @@ export default function EditarCategoria() {
         try {
           const data = await findById(Number(categoriaId));
           setCategoria(data);
+          setFormData({ nome: data.nome });
         } catch (error: unknown) {
           let msg = "Erro ao excluir categoria";
           if (axios.isAxiosError(error) && error.response) {
@@ -34,11 +44,90 @@ export default function EditarCategoria() {
     fetchCategoriaData();
   }, [categoriaId, navigate]);
 
+  function handleFormChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    try {
+      const requestBody = {
+        nome: formData.nome,
+      } as CategoriaDTO;
+
+      if (categoriaId) {
+        requestBody.id = Number(categoriaId);
+      }
+
+      await update(requestBody);
+      setSuccess("Categoria atualizada com sucesso.");
+      setTimeout(() => navigate("/categorias"), 3000);
+    } catch (error: unknown) {
+      let msg = "Erro ao carregar Categoria.";
+
+      if (axios.isAxiosError(error) && error.response && error.response.data) {
+        if (
+          error.response.data.errors &&
+          Array.isArray(error.response.data.errors)
+        ) {
+          const errorMessages = error.response.data.errors
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            .map((e: any) => e.message)
+            .join(", ");
+          msg = `Dados inválidos: ${errorMessages}. Tente novamente.`;
+        } else {
+          msg = error.response.data.error || msg;
+        }
+      }
+      setError(msg);
+      setTimeout(() => setError(null), 3000);
+    }
+  }
+
   return (
     <Box sx={{ mt: 2, p: 4 }}>
+      {success && (
+        <Alert severity="success" sx={{ mb: 2 }}>
+          {success}
+        </Alert>
+      )}
+
+      {error && (
+        <Alert severity="error" sx={{ mb: 2 }}>
+          {error}
+        </Alert>
+      )}
+
       <Typography variant="h4" component="h1">
         Editar Categoria
       </Typography>
+
+      <Box component="form" noValidate sx={{ mt: 2 }} onSubmit={handleSubmit}>
+        <TextField
+          id="nome"
+          name="nome"
+          label="Nome da Categoria"
+          value={formData.nome}
+          onChange={handleFormChange}
+          variant="outlined"
+          fullWidth
+          sx={{ mb: 2 }}
+        />
+        <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 2 }}>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => navigate("/categorias")}
+          >
+            Cancelar
+          </Button>
+          <Button variant="outlined" color="primary" type="submit">
+            Salvar
+          </Button>
+        </Box>
+      </Box>
     </Box>
   );
 }
--- frontend/src/services/categoria-service.ts
+++ frontend/src/services/categoria-service.ts
@@ -3,19 +3,24 @@ import type { CategoriaDTO } from "../models/categoria";
 import { BASE_URL } from "../utils/system";
 
 export async function findAll(): Promise<CategoriaDTO[]> {
+  const response = await axios.get(`${BASE_URL}/categorias`);
 
-    const response = await axios.get(`${BASE_URL}/categorias`);
-
-    return response.data;
-    
+  return response.data;
 }
 
-export async function deleteById( id: number){
-    await axios.delete(`${BASE_URL}/categorias/${id}`) ;
+export async function deleteById(id: number) {
+  await axios.delete(`${BASE_URL}/categorias/${id}`);
 }
 
 export async function findById(id: number): Promise<CategoriaDTO> {
+  const response = await axios.get(`${BASE_URL}/categorias/${id}`);
+  return response.data;
+}
 
-    const response = await axios.get(`${BASE_URL}/categorias/${id}`);
-    return response.data;
+export async function update(categoria: CategoriaDTO) {
+  const response = await axios.put(
+    `${BASE_URL}/categorias/${categoria.id}`,
+    categoria
+  );
+  return response.data;
 }
